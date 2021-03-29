94afd01c2bc0e546567060d078578f6844db8b7a
hemanth <hemanth.karnati@delta.com>
2021-03-29 15:58:00 -0400

    Rest client working commit

--- src/main/java/com/delta/rm/resource/ExternalResource.java
+++ src/main/java/com/delta/rm/resource/ExternalResource.java
@@ -7,6 +7,9 @@ import org.slf4j.LoggerFactory;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 @Path("/external")
 public class ExternalResource {
@@ -24,6 +27,13 @@ public class ExternalResource {
     @Path("/timeout")
     public String getMessageFromExternalApiTimeOut() {
         return service.invokeExternalApiTimeOut();
+    }
+
+    @GET
+    @Path("/offers/{offerId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getOffersFromExternal(@PathParam("offerId") String offerId) {
+        return service.invokeRetailOfferApi(offerId);
     }
 
 }
--- src/main/java/com/delta/rm/service/ExternalService.java
+++ src/main/java/com/delta/rm/service/ExternalService.java
@@ -1,5 +1,6 @@
 package com.delta.rm.service;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
 import org.eclipse.microprofile.faulttolerance.Fallback;
 import org.eclipse.microprofile.faulttolerance.Retry;
@@ -8,13 +9,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MediaType;
 import java.util.Random;
+import java.util.UUID;
 import java.util.concurrent.atomic.AtomicLong;
 
 @ApplicationScoped
 public class ExternalService {
     private static final Logger LOGGER = LoggerFactory.getLogger(ExternalService.class);
+
+    @ConfigProperty(name = "retail.offer.api")
+    String retailOfferApi;
+
     private AtomicLong counter = new AtomicLong(0);
+
+    private Client client = ClientBuilder.newClient();
 
     @Retry(maxRetries = 4)
     @CircuitBreaker(delay = 5000, requestVolumeThreshold = 4, failureRatio = 0.5)
@@ -54,5 +65,14 @@ public class ExternalService {
 
     private void randomDelay() throws InterruptedException {
         Thread.sleep(new Random().nextInt(500));
+    }
+
+    public String invokeRetailOfferApi(String offerId) {
+        return client
+                .target(retailOfferApi+offerId)
+                .request(MediaType.APPLICATION_JSON)
+                .header("Content-Type", MediaType.APPLICATION_JSON)
+                .header("transactionId", UUID.randomUUID().toString())
+                .get(String.class);
     }
 }
